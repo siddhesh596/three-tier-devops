22d5c47e1e6cf03a37ba578bd9cab8bae9ea9b16
Siddhesh Pawar <siddheshpawar2207@gmail.com>
2026-07-24 18:25:38 +0530

    Added Terraform base configuration

---
+++ three-tier-devops/terraform/variables.tf
@@ -0,0 +1,14 @@
+variable "aws_region" {
+  description = "AWS Region"
+  type        = string
+}
+
+variable "project_name" {
+  description = "Project Name"
+  type        = string
+}
+
+variable "environment" {
+  description = "Environment"
+  type        = string
+}
